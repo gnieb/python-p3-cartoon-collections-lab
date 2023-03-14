98391d7a4ac6b80afbb17bdb93fc974448fb9988
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-14 11:47:22 -0400

    added alternate solution for all problems

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,24 +1,65 @@
 def roll_call_dwarves(list):
     
-    for number, dwarf in enumerate (list, 1):
-        print(number, '. ', dwarf, sep='')
+    # for number, dwarf in enumerate (list, 1):
+    #     print(number, '. ', dwarf, sep='')
+
+        # OR
+    the_count = 1
+    for dwarf in list:
+        print( f"{the_count}. {dwarf}")
+        the_count += 1
+
 
 def summon_captain_planet(calls):
-    changed_calls = [call.capitalize() + '!' for call in calls]
-    return changed_calls
+    # changed_calls = [call.capitalize() + '!' for call in calls]
+    # return changed_calls
+        
+        #OR
+
+    new_list = []
+    for word in calls:
+        new_string = f"{word.capitalize()}!"
+        new_list.append(new_string)
+    return new_list
+
+
 
 def long_planeteer_calls(list):
-    long_calls = [call for call in list if len(call) > 4]
+    # long_calls = [call for call in list if len(call) > 4]
     
-    if len(long_calls) > 0:
-        return True
-    else:
-        return False
+    # if len(long_calls) > 0:
+    #     return True
+    # else:
+    #     return False
+    
+        #OR
+
+    for call in list:
+        if len(call) > 4:
+            return True
+    return False
+        
+
+
+
 
 def find_the_cheese(list):
-    cheesy_list = [food for food in list if (food == "cheddar" or food == "gouda" or food == "camembert")]
-    for cheese in cheesy_list:
-        return cheese
+    # cheesy_list = [food for food in list if (food == "cheddar" or food == "gouda" or food == "camembert")]
+    # for cheese in cheesy_list:
+    #     return cheese
+    
+    #OR
 
-find_the_cheese(["crackers", "gouda", "thyme"])
+    the_cheeses =["cheddar", "gouda", "camembert"]
 
+    for the_snack in list:
+        if the_snack in the_cheeses:
+            return the_snack
+
+
+
+
+
+# find_the_cheese(["crackers", "gouda", "thyme"])
+# roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+long_planeteer_calls(["earth", "wind", "fire", "water", "heart"])
